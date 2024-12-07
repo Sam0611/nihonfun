cf0f194be46d96cc15b798a18acc1752cbdcd6ac
Sam0611 <115499199+Sam0611@users.noreply.github.com>
2024-12-07 13:09:40 +0100

    Delete JS directory

--- JS/client_conf.js
+++
@@ -1,2 +0,0 @@
-// var sock = new WebSocket("wss://nihongo.samwebsite.fr:8444");
-var sock = new WebSocket("ws://localhost:5500");
--- JS/data.js
+++
@@ -1,6 +0,0 @@
-var img = [
-    "Akame.png", "Anya_Forger.png", "Chainsaw_Man.png", "Chitoge.png", "Decim.png", "Erza_Scarlet.png", "Faye.png", 
-    "FMAcreature.png", "Fuu.png", "Haru.png", "Jin.png", "Juuzou.png", "Koro.png", "Light_Yagami.png", "Loid_Forger.png", "Luffy.png", 
-    "Magic_Kaito.png", "Moka.png", "Mugen.png", "Naruto_sexy_jutsu.png", "Onizuka.png", "Pico.png", "Power.png", "Rock_Lee.png", "Ryuk.png",
-    "Shinichi_Kudo.png", "Shou_Tuker.png", "Soma_Yukihira.png", "Son_Goku.png", "Spike.png", "Yor_Briar.png", "Yumeko_Jabami.png", "Yuno_Gasai.png"
-];
