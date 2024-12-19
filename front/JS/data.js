6ddcf93a6916dcccc9d096925b4e44c2aaf0b2eb
Sam0611 <115499199+Sam0611@users.noreply.github.com>
2024-12-19 21:14:23 +0100

    hxh pictures added in data.js

--- front/JS/data.js
+++ front/JS/data.js
@@ -1,6 +1,6 @@
 var img = [
-    "Akame.png", "Anya_Forger.png", "Chainsaw_Man.png", "Chitoge.png", "Decim.png", "Erza_Scarlet.png", "Faye.png", 
-    "FMAcreature.png", "Fuu.png", "Haru.png", "Jin.png", "Juuzou.png", "Koro.png", "Light_Yagami.png", "Loid_Forger.png", "Luffy.png", 
-    "Magic_Kaito.png", "Moka.png", "Mugen.png", "Naruto_sexy_jutsu.png", "Onizuka.png", "Pico.png", "Power.png", "Rock_Lee.png", "Ryuk.png",
+    "Akame.png", "Anya_Forger.png", "Chainsaw_Man.png", "Chitoge.png", "Decim.png", "Djido.png", "Erza_Scarlet.png", "Faye.png", 
+    "FMAcreature.png", "Fuu.png", "Gon.png", "Haru.png", "Hisoka.png", "Jin.png", "Juuzou.png", "Kirua.png", "Koro.png", "Light_Yagami.png", "Loid_Forger.png", "Luffy.png", 
+    "Magic_Kaito.png", "Moka.png", "Mugen.png", "Naruto_sexy_jutsu.png", "Neferpitou.png", "Onizuka.png", "Pico.png", "Power.png", "Rock_Lee.png", "Ryuk.png",
     "Shinichi_Kudo.png", "Shou_Tuker.png", "Soma_Yukihira.png", "Son_Goku.png", "Spike.png", "Yor_Briar.png", "Yumeko_Jabami.png", "Yuno_Gasai.png"
 ];
